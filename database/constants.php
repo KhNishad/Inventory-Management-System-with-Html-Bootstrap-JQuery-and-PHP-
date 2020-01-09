a668c06678774428d22cb720bd5b295930cdd4da
Abdus Sattar (Nishad) <50777616+KhNishad@users.noreply.github.com>
2020-01-09 14:08:04 +0600

    Add files via upload

---
+++ database/constants.php
@@ -0,0 +1,10 @@
+<?php
+session_start();
+define("HOST","localhost");
+define("USER","root");
+define("PASS","");
+define("DB","project_inv");
+
+define("DOMAIN", "http://localhost/inventory_project2/public_html");
+
+?>
